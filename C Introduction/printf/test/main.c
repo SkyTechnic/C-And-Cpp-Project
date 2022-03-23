52054d7261816c1fb4ecfef2dce2e5fd696e2041
Steve Shen <steveshencn@gmail.com>
2022-03-23 17:08:35 +0800

    Add sum Function

--- C Introduction/printf/test/main.c
+++ C Introduction/printf/test/main.c
@@ -1,5 +1,14 @@
 #include<stdio.h>
-
+int sum(int num)
+{
+   int sum=0;
+   for (int i = 0; i < num; i++)
+   {
+      /* code */
+      sum+=i;
+   }
+   return sum;
+}
 int main()
 {
    char ch = 'A';
@@ -14,5 +23,7 @@ int main()
    printf("双精度值为 %#xlf \n", dbl);
    printf("八进制值为 %o \n", no);
    printf("十六进制值为 %x \n", no);
+   printf("整数为 %-5d\n" , sum(no));
+
    return 0;
 }
